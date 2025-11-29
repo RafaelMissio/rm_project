40721a3f0ec5b51b5654aa8421811ad456e0281f
Rafael Missio <rtmissio@gmail.com>
2025-11-29 01:28:02 -0300

    createade controller cliet

--- src/main/java/br/com/missio/project/web/clients/controllers/ClientController.java
+++ src/main/java/br/com/missio/project/web/clients/controllers/ClientController.java
@@ -1,32 +1,28 @@
 package br.com.missio.project.web.clients.controllers;
 
-import br.com.missio.project.core.entities.Client;
-import br.com.missio.project.core.services.ClientService;
-import br.com.missio.project.web.clients.dto.ClientDTO;
+import br.com.missio.project.core.repositories.ClientRepository;
+import br.com.missio.project.web.clients.dto.ClientViewModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
 import java.util.Map;
+
 
 @Controller
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class ClientController {
 
-    private final ClientService clientService;
+    private final ClientRepository clientRepository;
 
-    @RequestMapping("/client")
-    public ModelAndView index(){
-     var clients = clientService.findAll();
-     var clientsDTO = new ArrayList<ClientDTO>();
-     for (Client client : clients) {
-         var clientDTO = ClientDTO.of(client);
-            clientsDTO.add(clientDTO);
-     }
-     var model = Map.of("clients", clientsDTO);
-     return new ModelAndView("clients/index", model);
+    @RequestMapping("/clients")
+    public ModelAndView index() {
+        var clients = clientRepository.findAll()
+                .stream()
+                .map(ClientViewModel::of)
+                .toList();
+        var model = Map.of("clients", clients);
+        return new ModelAndView("clients/index", model);
     }
 }
